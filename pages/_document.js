536f2c5f33b22a0786664ef284e9b8c7b958062f
alirezayoom <alirezayoom@gmail.com>
2023-01-13 07:33:41 +0430

    document added

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Freehand&family=Inter:wght@100;200;300;400;700;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Questrial&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
