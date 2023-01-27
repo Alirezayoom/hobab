83d61fe6637dff3ccb44a49b14417c85a065b21a
alirezayoom <alirezayoom@gmail.com>
2023-01-27 21:58:39 +0430

    date justified center

--- components/Card.js
+++ components/Card.js
@@ -7,13 +7,12 @@ export default function Hobab({ posts }) {
         <div key={post.id} className={classes.container}>
           <div className={classes.box}>
             <div className={classes.blur}></div>
-            <div className={classes.number}>{post.id}</div>
+            {/* <div className={classes.name}>{post.name}</div>
+            <div className={classes.line}></div> */}
+
             <div className={classes.text}>{post.post}</div>
             <div className={classes.line}></div>
-            <div className={classes.dateAuthor}>
-              <div className={classes.date}>{post.date}</div>
-              <div className={classes.name}>{post.name}</div>
-            </div>
+            <div className={classes.date}>{post.date}</div>
           </div>
         </div>
       ))}
